242d42cba66b41575d3eae42b23dd91fa138311b
Sreja1985 <77894225+Sreja1985@users.noreply.github.com>
2022-05-09 20:04:54 +0200

    Update Departments.js

--- server/models/Departments.js
+++ server/models/Departments.js
@@ -1,1 +1,28 @@
 
+module.exports = (sequelize, DataTypes) => {
+    const Departments = sequelize.define("Departments", {
+        departmentTitle: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        alias: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+
+    Departments.associate = (models) => {
+
+        Departments.belongsTo(models.Regions, {
+            foreignKey: 'RegionId',
+            onDelete: "cascade",
+        });
+
+        Departments.hasMany(models.Numbers, {
+            foreignKey: 'DepartmentId',
+            onDelete: "cascade",
+        });
+    };
+  
+    return Departments;
+};
