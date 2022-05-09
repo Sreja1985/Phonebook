65ec17c88bef7b30a2132c9b8a3941510077ed32
Sreja1985 <77894225+Sreja1985@users.noreply.github.com>
2022-05-09 19:47:52 +0200

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../CSS/App.css'
+
+export default function Footer() {
+
+
+  return (
+    <>
+      <div className="footer">
+        Srećko Ljubić
+        <Link to="/registracija">©</Link>
+        2022
+      </div>
+    </>
+  )
+}
 
