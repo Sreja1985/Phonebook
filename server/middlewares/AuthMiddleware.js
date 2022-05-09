5d68d60579e8310cda0fad09d6095201072bd2bf
Sreja1985 <77894225+Sreja1985@users.noreply.github.com>
2022-05-09 20:03:13 +0200

    Update AuthMiddleware.js

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -1,1 +1,20 @@
+const { verify } = require('jsonwebtoken');
 
+const validateToken = (req, res, next) => {
+    const accessToken = req.header("accessToken");//dohvača ga s fronthenda
+
+    if (!accessToken) return res.json({ error: "Korisnik nije prijavljen !" });
+
+    try {
+        const validToken = verify(accessToken, "token");
+        if (validToken) {
+            return next();
+        }
+    }
+    catch (error) {
+        return res.json({ error: error });
+    }
+
+};
+
+module.exports = { validateToken };
